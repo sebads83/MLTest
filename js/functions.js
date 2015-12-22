880caa76140770b9daa30bea6ca25a8b2627f5a1
Sebastian Dos Santos <abismo@gmail.com>
2015-12-22 01:10:58 -0300

    Customizacion elementos desktop mobile y funcionalidades

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,14 @@
 /* functions */
-var hola = function() {
-	console.log($j(window).width());
+var reinitZoom = function() {
+	var zoom = '';
+	// var _width = $j(window).width();
+	
+	// if(_width <= 840) {
+	// 	$j('#myZoom').zoom.disbale();
+	// } 
+	// if(_width >= 841) {
+	// 	$j('#myZoom').zoom.enable();
+	// }
+
+	
 }
--- js/init.js
+++ js/init.js
@@ -5,6 +5,9 @@ var $j = jQuery.noConflict();
 /* Script Execution */
 
 $j(document).ready(function() {
-	var carousel = new ch.Carousel(myCarousel);
 	var zoom = new ch.Zoom(myZoom);
+	
+	var carousel = new ch.Carousel(myCarousel,[
+			limitPerPage= 3
+		]);
 });
